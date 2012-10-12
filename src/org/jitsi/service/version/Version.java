5130203235936e4abac517606c136cdf9a033f04
Emil Ivov <emcho@jitsi.org>
2012-10-12 17:48:03 +0000

    Change versioning to allow for an easier integration of SVN revision and build number. Removes version revision leaving the version to major.minor[.build-label] where build label may include things like svn revisions, dates, etc.

--- src/org/jitsi/service/version/Version.java
+++ src/org/jitsi/service/version/Version.java
@@ -7,8 +7,8 @@
 package org.jitsi.service.version;
 
 /**
- * Contains version information of the SIP Communicator instance that we're
- * currently running.
+ * Contains version information of the Jitsi instance that we're currently
+ * running.
  *
  * @author Emil Ivov
  * @author Lyubomir Marinov
@@ -35,48 +35,38 @@ public interface Version
         = "sip-communicator.version";
 
     /**
-     * Returns the version major of the current SIP Communicator version. In an
+     * Returns the version major of the current Jitsi version. In an
      * example 2.3.1 version string 2 is the version major. The version major
      * number changes when a relatively extensive set of new features and
-     * possibly rearchitecturing have been applied to the SIP Communicator.
+     * possibly rearchitecturing have been applied to the Jitsi.
      *
      * @return the version major integer.
      */
     public int getVersionMajor();
 
     /**
-     * Returns the version minor of the current SIP Communicator version. In an
+     * Returns the version minor of the current Jitsi version. In an
      * example 2.3.1 version string 3 is the version minor. The version minor
      * number changes after adding enhancements and possibly new features to a
-     * given SIP Communicator version.
+     * given Jitsi version.
      *
      * @return the version minor integer.
      */
     public int getVersionMinor();
 
     /**
-     * Returns the version revision number of the current SIP Communicator
-     * version. In an example 2.3.1 version string 1 is the revision number.
-     * The version revision number number changes after applying bug fixes and
-     * possible some small enhancements to a given SIP Communicator version.
-     *
-     * @return the version revision number.
-     */
-    public int getVersionRevision();
-
-    /**
-     * Indicates if this SIP Communicator version corresponds to a nightly build
-     * of a repository snapshot or to an official SIP Communicator release.
+     * Indicates if this Jitsi version corresponds to a nightly build
+     * of a repository snapshot or to an official Jitsi release.
      *
      * @return true if this is a build of a nightly repository snapshot and
-     * false if this is an official SIP Communicator release.
+     * false if this is an official Jitsi release.
      */
     public boolean isNightly();
 
     /**
      * If this is a nightly build, returns the build identifies (e.g.
-     * nightly-2007.12.07-06.45.17). If this is not a nightly build SIP
-     * Communicator version, the method returns null.
+     * nightly-2007.12.07-06.45.17). If this is not a nightly build Jitsi
+     * version, the method returns null.
      *
      * @return a String containing a nightly build identifier or null if
      */
@@ -90,10 +80,11 @@ public interface Version
     public boolean isPreRelease();
 
     /**
-     * Returns the version prerelease ID of the current SIP Communicator version
+     * Returns the version prerelease ID of the current Jitsi version
      * and null if this version is not a prerelease. Version pre-release id-s
-     * and version revisions are exclusive, so in case this version is a pre-
-     * release the revision will bereturn null
+     * exist only for pre-releaseversions and are <tt>null<tt/> otherwise. Note
+     * that pre-relesae versions are not used by Jitsi's current versioning
+     * convention
      *
      * @return a String containing the version prerelease ID.
      */
@@ -116,33 +107,33 @@ public interface Version
 
     /**
      * Compares the <tt>version</tt> parameter to this version and returns true
-     * if and only if both reference the same SIP Communicator version and
+     * if and only if both reference the same Jitsi version and
      * false otherwise.
      *
      * @param version the version instance that we'd like to compare with this
      * one.
      * @return true if and only the version param references the same
-     * SIP Communicator version as this Version instance and false otherwise.
+     * Jitsi version as this Version instance and false otherwise.
      */
     public boolean equals(Object version);
 
     /**
      * Returns the name of the application that we're currently running. Default
-     * MUST be SIP Communicator.
+     * MUST be Jitsi.
      *
      * @return the name of the application that we're currently running. Default
-     * MUST be SIP Communicator.
+     * MUST be Jitsi.
      */
     public String getApplicationName();
 
     /**
      * Returns a String representation of this Version instance. If you'd just
-     * like to obtain the version of SIP Communicator so that you could display
+     * like to obtain the version of Jitsi so that you could display
      * it (e.g. in a Help->About dialog) then all you need is calling this
      * method.
      *
-     * @return a major.minor.revision[.build] String containing the complete
-     * SIP Communicator version.
+     * @return a major.minor[.build] String containing the complete
+     * Jitsi version.
      */
     public String toString();
 }
