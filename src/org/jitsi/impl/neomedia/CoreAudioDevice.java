81b7d1f6a09be2c54f4ae1e0ddf57a5d2e87fb68
Vincent Lucas <chenzo@jitsi.org>
2013-12-24 17:51:47 +0100

    Log information to determine why the CoreAudio library does not load correctly from the build.

--- src/org/jitsi/impl/neomedia/CoreAudioDevice.java
+++ src/org/jitsi/impl/neomedia/CoreAudioDevice.java
@@ -59,12 +59,15 @@ public class CoreAudioDevice
              * to not prevent the loading of its users and isLoaded will remain
              * false eventually.
              */
+            logger.info("Failed to load CoreAudioDevice library: " + npe);
         }
         catch (SecurityException se)
         {
+            logger.info("Failed to load CoreAudioDevice library: " + se);
         }
         catch (UnsatisfiedLinkError ule)
         {
+            logger.info("Failed to load CoreAudioDevice library: " + ule);
         }
 
         CoreAudioDevice.isLoaded = isLoaded;
