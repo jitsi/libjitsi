9cc18ecf3d3a11621610ca993ec022b065d46870
Kuntz Thomas <thomaskuntz@sharedmemory.fr>
2014-07-15 16:12:45 +0200

    Use the difference between the timestamp of 2 consecutive frames read in the IVF file to sync the stream, instead of just using the framerate of the frame.

--- src/org/jitsi/impl/neomedia/jmfext/media/protocol/ivffile/IVFStream.java
+++ src/org/jitsi/impl/neomedia/jmfext/media/protocol/ivffile/IVFStream.java
@@ -32,16 +32,29 @@ public class IVFStream
     private long timeLastRead = 0;
     
     /**
+     * The timestamp of the frame (of its header) read during the last call
+     * to doRead. It's used to determine if enough time has passed during the
+     * last frame and the next frame to be returned.
+     */
+    private long lastFrameTimestamp = 0;
+    
+    /**
+     * The VP8Frame that is filled by the VP8 frame and header read in the IVF
+     * file. We use the same VP8Frame object (set with the new frame each time
+     * of course) to avoid the allocation of a new VP8Frame each time.
+     */
+    private VP8Frame frame = new VP8Frame();
+    
+    /**
      * The <tt>IVFFileReader</tt> used to get the frame of the IVF file.
      */
     private IVFFileReader ivfFileReader;
     
     /**
-     * The framerate of the video stream.
-     * It start with a value of 1 (to avoid division by zero) but will take
-     * the framerate of the IVF file.
+     * The timebase of the video stream (in millisecondes).
+     * TIMEBASE = 1000 * timescale / framerate = 1000 / ( framerate / timescale )
      */
-    private float FRAMERATE = 1;
+    private int TIMEBASE;
     
     
     /**
@@ -59,7 +72,7 @@ public class IVFStream
         this.ivfFileReader = new IVFFileReader(
                 dataSource.getLocator().getRemainder());
         
-        this.FRAMERATE = ((VideoFormat)getFormat()).getFrameRate();
+        this.TIMEBASE = (int)(1000. / ((VideoFormat)getFormat()).getFrameRate());
     }
 
     
@@ -88,19 +101,32 @@ public class IVFStream
                 buffer.setFormat(format);
         }
                 
-        byte[] data = ivfFileReader.getNextFrame(true);
+        ivfFileReader.getNextFrame(frame,true);
         
-        buffer.setData(data);
+        buffer.setData(frame.getFrameData());
         buffer.setOffset(0);
-        buffer.setLength(data.length);
+        buffer.setLength(frame.getFrameLength());
         
         
         buffer.setTimeStamp(System.nanoTime());
         buffer.setFlags(Buffer.FLAG_SYSTEM_TIME | Buffer.FLAG_LIVE_DATA);
         
         
+        /*
+         * We just check if the time that has passed since the last call to doRead
+         * took more or less milliseconds than the number of milliseconds
+         * between 2 consecutive frames (based on their timestamps and the
+         * the timebase). If it's less, we wait the remaining
+         * time (and if its more, we directly return).
+         */
         millis = System.currentTimeMillis() - this.timeLastRead;
-        millis = (long)(1000.0 / this.FRAMERATE) - millis;
+        millis = (frame.getTimestamp() - lastFrameTimestamp) * TIMEBASE - millis;
+        /**
+         * Even if we loop the IVF file, there won't be any problem with millis
+         * when the last frame of the file was reached and the current frame is
+         * the first one : (frame.getTimestamp() - lastFrameTimestamp) will
+         * be negative so we won't sleep this time.
+         */
         if(millis > 0)
         {
             try
@@ -112,6 +138,7 @@ public class IVFStream
                 e.printStackTrace();
             }
         }
+        this.lastFrameTimestamp = frame.getTimestamp();
         this.timeLastRead=System.currentTimeMillis();
     }
 }
