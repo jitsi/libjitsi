53488ce3242017f631f1b482c441a54c335bf2a6
George Politis <gp@jitsi.org>
2014-07-22 13:34:13 +0200

    Adds missing license header.

--- src/org/jitsi/impl/neomedia/rtcp/termination/strategies/FeedbackCacheEntry.java
+++ src/org/jitsi/impl/neomedia/rtcp/termination/strategies/FeedbackCacheEntry.java
@@ -1,3 +1,9 @@
+/*
+ * Jitsi, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
 package org.jitsi.impl.neomedia.rtcp.termination.strategies;
 
 import net.sf.fmj.media.rtp.*;
