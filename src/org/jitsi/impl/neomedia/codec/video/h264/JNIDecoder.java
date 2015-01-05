a269725e7bae01d84c48aaa4499663986bd74ce9
Paul Gregoire <mondain@gmail.com>
2015-01-05 10:26:31 -0500

    Input buffer timestamp used instead of setting as unknown
    Created as an addon for issue #28

--- src/org/jitsi/impl/neomedia/codec/video/h264/JNIDecoder.java
+++ src/org/jitsi/impl/neomedia/codec/video/h264/JNIDecoder.java
@@ -431,7 +431,7 @@ public class JNIDecoder
 
         if (pts == FFmpeg.AV_NOPTS_VALUE)
         {
-            out.setTimeStamp(Buffer.TIME_UNKNOWN);
+            out.setTimeStamp(in.getTimeStamp());
         }
         else
         {
