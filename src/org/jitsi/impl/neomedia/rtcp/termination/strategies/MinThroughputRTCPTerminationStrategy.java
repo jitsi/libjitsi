276a315c91c3ba888c2e02109912f8dc074d6bcb
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2015-10-29 16:10:57 -0500

    Fixes MinThroughputRTCPTerminationStrategy (which got broken by the introduction and/or operation of SsrcRewritingEngine).

--- src/org/jitsi/impl/neomedia/rtcp/termination/strategies/MinThroughputRTCPTerminationStrategy.java
+++ src/org/jitsi/impl/neomedia/rtcp/termination/strategies/MinThroughputRTCPTerminationStrategy.java
@@ -15,14 +15,7 @@
  */
 package org.jitsi.impl.neomedia.rtcp.termination.strategies;
 
-import net.sf.fmj.media.rtp.*;
-
-import net.sf.fmj.media.rtp.util.*;
-import org.jitsi.impl.neomedia.*;
 import org.jitsi.impl.neomedia.rtcp.*;
-import org.jitsi.impl.neomedia.transform.*;
-import org.jitsi.service.neomedia.*;
-import org.jitsi.util.function.*;
 
 /**
  * Minimizes endpoint throughput. It does that by sending REMB messages with the
@@ -30,108 +23,34 @@ import org.jitsi.util.function.*;
  * used in tests.
  *
  * @author George Politis
+ * @author Lyubomir Marinov
  */
 public class MinThroughputRTCPTerminationStrategy
-    implements RTCPTerminationStrategy
+    extends BasicRTCPTerminationStrategy
 {
-    private final PacketTransformer rtcpTransformer
-        = new SinglePacketTransformer()
-    {
-        /**
-         * The minimum value of the mantissa in the REMB calculation.
-         */
-        private static final int MIN_MANTISSA = 10;
+    /**
+     * The minimum value of the exponent in the REMB calculation.
+     */
+    private static final int MIN_EXP = 1;
 
-        /**
-         * The minimum value of the exponent in the REMB calculation.
-         */
-        private static final int MIN_EXP = 1;
+    /**
+     * The minimum value of the mantissa in the REMB calculation.
+     */
+    private static final int MIN_MANTISSA = 10;
 
-        /**
-         * The parser that parses <tt>RawPacket</tt>s to
-         * <tt>RTCPCompoundPacket</tt>s.
-         */
-        private final RTCPPacketParserEx parser = new RTCPPacketParserEx();
-
-        /**
-         * The generator that generates <tt>RawPacket</tt>s from
-         * <tt>RTCPCompoundPacket</tt>s.
-         */
-        private final RTCPGenerator generator = new RTCPGenerator();
-
-        @Override
-        public RawPacket transform(RawPacket pkt)
-        {
-            if (pkt == null)
-            {
-                return null;
-            }
-
-            RTCPCompoundPacket compoundPkt;
-
-            try
-            {
-                compoundPkt
-                    = (RTCPCompoundPacket)
-                        parser.parse(
-                                pkt.getBuffer(),
-                                pkt.getOffset(),
-                                pkt.getLength());
-            }
-            catch (BadFormatException e)
-            {
-                return null;
-            }
-
-            if (compoundPkt == null
-                    || compoundPkt.packets == null
-                    || compoundPkt.packets.length == 0)
-            {
-                return pkt;
-            }
-
-            for (RTCPPacket p : compoundPkt.packets)
-            {
-                switch (p.type)
-                {
-                case RTCPFBPacket.PSFB:
-                    RTCPFBPacket psfb = (RTCPFBPacket) p;
-
-                    switch (psfb.fmt)
-                    {
-                    case RTCPREMBPacket.FMT:
-                        RTCPREMBPacket remb = (RTCPREMBPacket) p;
-
-                        remb.mantissa = MIN_MANTISSA;
-                        remb.exp = MIN_EXP;
-                        break;
-                    }
-                    break;
-                }
-            }
-
-            // We've modified compoundPkt so we have to reassemble it only.
-            return generator.apply(compoundPkt);
-        }
-
-        @Override
-        public RawPacket reverseTransform(RawPacket pkt)
-        {
-            return pkt;
-        }
-    };
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public PacketTransformer getRTCPTransformer()
+    protected RTCPREMBPacket makeRTCPREMBPacket()
     {
-        // Replace the mantissa and the exponent in the REMB packets.
-        return rtcpTransformer;
-    }
+        RTCPREMBPacket remb = super.makeRTCPREMBPacket();
 
-    @Override
-    public PacketTransformer getRTPTransformer()
-    {
-        // We don't touch the RTP traffic.
-        return null;
+        if (remb != null)
+        {
+            remb.mantissa = MIN_MANTISSA;
+            remb.exp = MIN_EXP;
+        }
+        return remb;
     }
 }
