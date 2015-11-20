be31336e46390d2653c4449ea9cb12979b79f293
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2015-11-20 16:02:38 -0600

    Removes the old implementation of SsrcRewriter.extendOriginalSequenceNumber in order to simplify the source code.

--- src/org/jitsi/impl/neomedia/transform/SsrcRewriter.java
+++ src/org/jitsi/impl/neomedia/transform/SsrcRewriter.java
@@ -27,18 +27,6 @@ import org.jitsi.service.neomedia.*;
 class SsrcRewriter
 {
     /**
-     * The median value of an unsigned short (2^15).
-     */
-    private static final int MEDIAN_UNSIGNED_SHORT = 32768;
-
-    /**
-     * We assume that if {@code RETRANSMISSION_FRONTIER_MS} have passed since we
-     * first saw a sequence number, then that sequence number won't be
-     * retransmitted.
-     */
-    private static final long RETRANSMISSION_FRONTIER_MS = 30 * 1000;
-
-    /**
      * The origin SSRC that this <tt>SsrcRewriter</tt> rewrites. The
      * target SSRC is managed by the parent <tt>SsrcGroupRewriter</tt>.
      */
@@ -111,13 +99,12 @@ class SsrcRewriter
     public RawPacket rewriteRTP(RawPacket pkt)
     {
         short seqnum = (short) pkt.getSequenceNumber();
-        int origExtendedSequenceNumber
-            = extendOriginalSequenceNumber(seqnum);
+        int extendedSeqnum = extendOriginalSequenceNumber(seqnum);
 
         // first, check if this is a retransmission and rewrite using
         // an appropriate interval.
         ExtendedSequenceNumberInterval retransmissionInterval
-            = findRetransmissionInterval(origExtendedSequenceNumber);
+            = findRetransmissionInterval(extendedSeqnum);
         boolean debug = SsrcRewritingEngine.logger.isDebugEnabled();
 
         if (retransmissionInterval != null)
@@ -142,7 +129,7 @@ class SsrcRewriter
             currentExtendedSequenceNumberInterval
                 = new ExtendedSequenceNumberInterval(
                         this,
-                        origExtendedSequenceNumber,
+                        extendedSeqnum,
                         ssrcGroupRewriter.currentExtendedSeqnumBase);
         }
         else
@@ -150,7 +137,7 @@ class SsrcRewriter
             // more packets to the stream, increase the sequence number interval
             // range.
             currentExtendedSequenceNumberInterval.extendedMaxOrig
-                = origExtendedSequenceNumber;
+                = extendedSeqnum;
             // the timestamp needs to be greater or equal to the maxTimestamp
             // for the current extended sequence number interval.
             currentExtendedSequenceNumberInterval.maxTimestamp = timestamp;
@@ -200,17 +187,17 @@ class SsrcRewriter
     {
         if (currentExtendedSequenceNumberInterval != null)
         {
-            intervals.put(currentExtendedSequenceNumberInterval.extendedMaxOrig,
-                currentExtendedSequenceNumberInterval);
-            // Store the max timestamp so that we can consult it when
-            // we rewrite the next packets of the next stream.
+            intervals.put(
+                    currentExtendedSequenceNumberInterval.extendedMaxOrig,
+                    currentExtendedSequenceNumberInterval);
+            // Store the max timestamp so that we can consult it when we rewrite
+            // the next packets of the next stream.
             ssrcGroupRewriter.maxTimestamp
                 = currentExtendedSequenceNumberInterval.maxTimestamp;
             currentExtendedSequenceNumberInterval = null;
 
-            // TODO We don't need to keep track of more than 2 cycles,
-            // so we need to trim the intervals tree to accommodate just
-            // that.
+            // TODO We don't need to keep track of more than 2 cycles, so we
+            // need to trim the intervals tree to accommodate just that.
         }
         else
         {
@@ -256,152 +243,6 @@ class SsrcRewriter
      * @return
      */
     int extendOriginalSequenceNumber(short ssOrigSeqnum)
-    {
-        int newValue = extendOriginalSequenceNumberNew(ssOrigSeqnum);
-
-        if (SsrcRewritingEngine.logger.isDebugEnabled())
-        {
-            int oldValue = extendOriginalSequenceNumberOld(ssOrigSeqnum);
-
-            if (oldValue != newValue)
-            {
-                logDebug(
-                        "The old implementation of extendOriginalSequenceNumber"
-                            + " says " + oldValue + " but the new one says "
-                            + newValue + "!");
-            }
-        }
-
-        return newValue;
-    }
-
-    /**
-     *
-     * @param ssOrigSeqnum
-     * @return
-     */
-    private int extendOriginalSequenceNumberOld(short ssOrigSeqnum)
-    {
-        // XXX we're using hungarian notation here to distinguish between signed
-        // short, unsigned short etc.
-
-        // Find the most recent extended sequence number interval for this SSRC.
-        ExtendedSequenceNumberInterval mostRecentInterval
-            = currentExtendedSequenceNumberInterval;
-
-        if (mostRecentInterval == null)
-        {
-            Map.Entry<Integer, ExtendedSequenceNumberInterval> entry
-                = intervals.lastEntry();
-
-            if (entry != null)
-            {
-                mostRecentInterval = entry.getValue();
-            }
-        }
-
-        int usOrigSeqnum = ssOrigSeqnum & 0x0000ffff;
-
-        if (mostRecentInterval == null)
-        {
-            // We don't have a most recent interval for this SSRC. This must be
-            // the very first RTP packet that we receive for this SSRC. The
-            // cycle is 0 and the extended sequence number is whatever the
-            // original sequence number is.
-            return usOrigSeqnum;
-        }
-
-        int extendedMaxOrig = mostRecentInterval.extendedMaxOrig;
-        int usMaxSeqnum = extendedMaxOrig & 0x0000ffff;
-        int cycles = extendedMaxOrig & 0xffff0000;
-
-        // There are two possible cases, either this is a re-transmission, or
-        // it's a new sequence number that will be used to either extend the
-        // current interval (if there is a current interval) or start a new one.
-        int delta = usOrigSeqnum - usMaxSeqnum;
-        boolean inRetransmissionFrontier
-            = System.currentTimeMillis() - mostRecentInterval.lastSeen
-                < RETRANSMISSION_FRONTIER_MS;
-
-        if (delta > 0)
-        {
-            // If the received sequence number (unsigned 16 bits) is bigger than
-            // the most recent max, then one of the following holds:
-            //
-            // 1. A new sequence number from this cycle. For example,
-            //    usOrigSeqnum = 60001 and usMaxSeqnum = 60000.
-            // 2. A new sequence number from a subsequent cycle. For example,
-            //    usOrigSeqnum = 60001 and usMaxSeqnum = 60000.
-            // 3. A retransmission from the previous cycle. For example,
-            // usOrigSeqnum = 65536 and usMaxSeqnum = 1
-            //
-            // If this is a packet from a subsequent cycle, then this means that
-            // the sequence numbers have advanced at least one cycle. Assuming
-            // that a cycle takes at least 30 seconds to complete (atm it takes
-            // ~ 20 // mins), then the mostRecentInterval must have been last
-            // touched more than 30s ago.
-
-            if (inRetransmissionFrontier)
-            {
-                // the last sequence number is recent.
-                if (delta > MEDIAN_UNSIGNED_SHORT)
-                {
-                    // retransmission from the previous cycle.
-                    cycles -= 0x10000;
-                }
-                else
-                {
-                    // new sequence number from this cycle.
-                }
-            }
-            else
-            {
-                // sequence number from _a_ subsequent cycle (not sure which
-                // one).
-                cycles += 0x10000;
-            }
-        }
-        else
-        {
-            // Else, the received sequence number (unsigned 16 bits) is smaller
-            // than the most recent max, then one of the following holds:
-            //
-            // 1. A new sequence number from _a_ subsequent cycle.
-            // 2. A retransmission from this cycle.
-
-            if (inRetransmissionFrontier)
-            {
-                // the last sequence number is recent
-                if (-delta > MEDIAN_UNSIGNED_SHORT)
-                {
-                    // if the distance to the last max is greater than 2^15,
-                    // then the sequence numbers must have wrapped around (new
-                    // cycle).
-                    cycles += 0x10000;
-                }
-                else
-                {
-                    // else, this is a retransmission from this cycle.
-                }
-            }
-            else
-            {
-                // this can't possibly be a retransmission as it would refer to
-                // something that's too old, so the sequence numbers must have
-                // wrapped around.
-                cycles += 0x10000;
-            }
-        }
-
-        return usOrigSeqnum + cycles;
-    }
-
-    /**
-     *
-     * @param ssOrigSeqnum
-     * @return
-     */
-    private int extendOriginalSequenceNumberNew(short ssOrigSeqnum)
     {
         SSRCCache ssrcCache
             = getMediaStream().getStreamRTPManager().getSSRCCache();
