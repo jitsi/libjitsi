33697002c1566eb3843fb45841446a06b77ed7b8
Boris Grozev <boris@jitsi.org>
2016-03-24 14:31:11 -0500

    Catches ArrayIndexOutOfBoundsException in isKeyFrame.

--- src/org/jitsi/impl/neomedia/codec/video/Utils.java
+++ src/org/jitsi/impl/neomedia/codec/video/Utils.java
@@ -32,44 +32,52 @@ public class Utils
      */
     public static boolean isKeyFrame(RawPacket pkt, Byte redPT, byte vp8PT)
     {
-        // XXX this will not work correctly when RTX gets enabled!
-        boolean isKeyFrame;
-        if (redPT != null && redPT == pkt.getPayloadType())
+        boolean isKeyFrame = false;
+        try
         {
-            REDBlock block = REDBlockIterator.getPrimaryBlock(pkt.getBuffer(),
-                    pkt.getPayloadOffset(), pkt.getPayloadLength());
-
-            if (block != null && vp8PT == block.getPayloadType())
+            // XXX this will not work correctly when RTX gets enabled!
+            if (redPT != null && redPT == pkt.getPayloadType())
             {
+                REDBlock block = REDBlockIterator
+                    .getPrimaryBlock(pkt.getBuffer(),
+                                     pkt.getPayloadOffset(),
+                                     pkt.getPayloadLength());
+
+                if (block != null && vp8PT == block.getPayloadType())
+                {
+                    // FIXME What if we're not using VP8?
+                    isKeyFrame
+                        = DePacketizer.isKeyFrame(
+                        pkt.getBuffer(),
+                        block.getOffset(),
+                        block.getLength());
+                }
+            }
+            else if (vp8PT == pkt.getPayloadType())
+            {
+                // XXX There's RawPacket#getPayloadLength() but the implementation
+
+                // includes pkt.paddingSize at the time of this writing and
+                // we do
+                // not know whether that's going to stay that way.
+
                 // FIXME What if we're not using VP8?
                 isKeyFrame
                     = DePacketizer.isKeyFrame(
-                            pkt.getBuffer(),
-                            block.getOffset(),
-                            block.getLength());
-            }
-            else
-            {
-                isKeyFrame = false;
+                    pkt.getBuffer(),
+                    pkt.getPayloadOffset(),
+                    pkt.getLength()
+                        - pkt.getHeaderLength()
+                        - pkt.getPaddingSize());
             }
         }
-        else if (vp8PT == pkt.getPayloadType())
+        catch (ArrayIndexOutOfBoundsException e)
         {
-            // XXX There's RawPacket#getPayloadLength() but the implementation
-            // includes pkt.paddingSize at the time of this writing and we do
-            // not know whether that's going to stay that way.
-
-            // FIXME What if we're not using VP8?
-            isKeyFrame
-                = DePacketizer.isKeyFrame(
-                        pkt.getBuffer(),
-                        pkt.getPayloadOffset(),
-                        pkt.getLength()
-                            - pkt.getHeaderLength()
-                            - pkt.getPaddingSize());
-        }
-        else
-        {
+            // While ideally we should check the bounds everywhere and not
+            // attempt to access the packet's buffer at invalid indexes, there
+            // are too many places where it could inadvertently happen. It's
+            // safer to return a default value of 'false' from this utility
+            // method than to risk killing a thread which doesn't expect this.
             isKeyFrame = false;
         }
 
