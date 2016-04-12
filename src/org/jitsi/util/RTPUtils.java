7e91c0f28910ff9fffd0cebdd734dbb48e4cef33
Boris Grozev <boris@jitsi.org>
2016-04-12 15:28:32 -0500

    Adds a utility method for getting the difference between two RTP sequence numbers.

---
+++ src/org/jitsi/util/RTPUtils.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright @ 2015 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.util;
+
+/**
+ * RTP-related static utility methods.
+ * @author Boris Grozev
+ */
+public class RTPUtils
+{
+    /**
+     * Returns the difference between two RTP sequence numbers (modulo 2^16).
+     * @return the difference between two RTP sequence numbers (modulo 2^16).
+     */
+    public static int sequenceNumberDiff(int a, int b)
+    {
+        int diff = a - b;
+
+        if (diff < -(1<<15))
+            diff += 1<<16;
+        else if (diff > 1<<15)
+            diff -= 1<<16;
+
+        return diff;
+    }
+}
