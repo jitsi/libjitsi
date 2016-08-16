50e477c9da0485379ea8d021d646e08b470ab805
Boris Grozev <boris@jitsi.org>
2016-08-15 19:36:03 -0500

    feat: Adds extended interfaces for MediaStream statistics.

---
+++ src/org/jitsi/service/neomedia/stats/BasicSendStreamStats.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright @ 2015 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.service.neomedia.stats;
+
+/**
+ * Extends {@link BasicStreamStats} with methods specific for streams that we
+ * send.
+ *
+ * @author Boris Grozev
+ */
+public interface BasicSendStreamStats
+    extends BasicStreamStats
+{
+    /**
+     * @return an estimate for the recent loss rate.
+     */
+    double getLossRate();
+}
