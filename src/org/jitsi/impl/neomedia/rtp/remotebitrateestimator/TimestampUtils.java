1013fa55528504ee20daf0cf18643458657480ef
ethan <ethan@highfive.com>
2017-01-05 11:08:51 -0800

    java doc

--- src/org/jitsi/impl/neomedia/rtp/remotebitrateestimator/TimestampUtils.java
+++ src/org/jitsi/impl/neomedia/rtp/remotebitrateestimator/TimestampUtils.java
@@ -1,12 +1,45 @@
+/*
+ * Copyright @ 2017 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jitsi.impl.neomedia.rtp.remotebitrateestimator;
 
+/**
+ * Helper class to determine timestamp order
+ * @author Ethan Lin
+ */
 class TimestampUtils
 {
+    /**
+     * Calculate the subtraction result of two long input as unsigned 32bit int.
+     *
+     * @param t1
+     * @param t2
+     * @return
+     */
     static long subtractAsUnsignedInt32(long t1, long t2)
     {
         return (t1 - t2) & 0xFFFFFFFFL;
     }
 
+    /**
+     * webrtc/modules/include/module_common_types.h
+     *
+     * @param timestamp
+     * @param prevTimestamp
+     * @return true if timestamp is newer
+     */
     static boolean isNewerTimestamp(long timestamp, long prevTimestamp)
     {
         // Distinguish between elements that are exactly 0x80000000 apart.
