9eadf3bab79e0f161b8cb57f01113f46eed75b13
Boris Grozev <boris@jitsi.org>
2017-02-13 14:32:32 -0600

    fix: Disables the OHB extension unless signaled.

--- src/org/jitsi/impl/neomedia/transform/OriginalHeaderBlockTransformEngine.java
+++ src/org/jitsi/impl/neomedia/transform/OriginalHeaderBlockTransformEngine.java
@@ -54,7 +54,7 @@ public class OriginalHeaderBlockTransformEngine
     /**
      * The ID of the OHB RTP header extension, or -1 if it is not enabled.
      */
-    private byte extensionID = 8;
+    private byte extensionID = -1;
 
     /**
      * Initializes a new {@link OriginalHeaderBlockTransformEngine} instance.
