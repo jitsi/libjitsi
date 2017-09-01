jitsi/libjitsi

commit fc767ef25f449cf36b7a1028c39d936cf5edc55c

an attempt at refactoring RTPEncodingDesc::applyFrameBoundsHeuristics to make it more readable

src/org/jitsi/util/RTPUtils.java
@@ -44,6 +44,7 @@ public class RTPUtils
      * (modulo 2^16).
      * @return result of the subtraction of one RTP sequence number from another
      * (modulo 2^16).
+     * FIXME(brian): why do we have this and sequenceNumberDiff?
      */
     public static int subtractNumber(int a, int b)
     {
@@ -177,6 +178,28 @@ public class RTPUtils
         int b2 = (0xFF & (buffer[offset + 1]));
         int b3 = (0xFF & (buffer[offset + 2]));
         return b1 << 16 | b2 << 8 | b3;
+    }
+
+    /**
+     * Returns the given integer masked to 16 bits
+     * @param value the integer to mask
+     * @return the value, masked to only keep the lower
+     * 16 bits
+     */
+    public static int as16Bits(int value)
+    {
+        return value & 0xFFFF;
+    }
+
+    /**
+     * Returns the given integer masked to 32 bits
+     * @param value the integer to mask
+     * @return the value, masked to only keep the lower
+     * 32 bits
+     */
+    public static long as32Bits(long value)
+    {
+        return value & 0xFFFFFFFF;
     }
 
     /**

src/org/jitsi/impl/neomedia/rtp/FrameDesc.java
@@ -163,6 +163,18 @@ public class FrameDesc
     }
 
     /**
+     * Returns whether or not the last sequence number of this frame
+     * is known (conclusively)
+     *
+     * @return true if we know the last sequence number of this frame, false
+     * otherwise
+     */
+    public boolean lastSequenceNumberKnown()
+    {
+        return end != -1;
+    }
+
+    /**
      * Sets the end sequence number of this source frame.
      *
      * @param end the end sequence number of this source frame.
@@ -181,6 +193,19 @@ public class FrameDesc
     {
         return start;
     }
+
+    /**
+     * Returns whether or not the first sequence number of this frame
+     * is known (conclusively)
+     *
+     * @return true if we know the first sequence number of this frame, false
+     * otherwise
+     */
+    public boolean firstSequenceNumberKnown()
+    {
+        return start != -1;
+    }
+
 
     /**
      * Sets the start sequence number of this source frame.

src/org/jitsi/impl/neomedia/rtp/RTPEncodingDesc.java
@@ -249,124 +249,128 @@ public class RTPEncodingDesc
     }
 
     /**
-     * Applies frame boundaries heuristics to frames a and b, assuming a
-     * predates/is older than b.
+     * Applies frame boundaries heuristics to frames olderFrame and
+     * newerFrame, assuming olderFrame predates/is older than newerFrame.
+     * Depending on the relationship of olderFrame and newerFrame, and what
+     * we know about olderFrame and newerFrame, we may be able to deduce
+     * the last expected sequence number for olderFrame and/or the first
+     * expected sequence number of newerFrame.
      *
-     * @param a the {@link FrameDesc} that comes before b.
-     * @param b the {@link FrameDesc} that comes after a.
+     * @param olderFrame the {@link FrameDesc} that comes before newerFrame.
+     * @param newerFrame the {@link FrameDesc} that comes after olderFrame.
      */
-    private static void applyFrameBoundsHeuristics(FrameDesc a, FrameDesc b)
+    private static void applyFrameBoundsHeuristics(
+            FrameDesc olderFrame,
+            FrameDesc newerFrame)
     {
-        int aLastSeqNum = a.getEnd(), bFirstSeqNum = b.getStart();
-        if (aLastSeqNum != -1 && bFirstSeqNum != -1)
+        if (olderFrame.lastSequenceNumberKnown() && newerFrame.lastSequenceNumberKnown())
         {
-            // No need for heuristics.
+            // We already know the last sequence number of olderFrame and the first
+            // sequence number of newerFrame, no need for further heuristics.
             return;
         }
-
-        long tsDiff = (b.getTimestamp() - a.getTimestamp()) & 0xFFFFFFFFL;
-        if (tsDiff > (1L << 30) && tsDiff < (-(1L << 30) & 0xFFFFFFFFL))
+        if (!TimestampUtils.isNewerTimestamp(
+                newerFrame.getTimestamp(), olderFrame.getTimestamp()))
         {
-            // the distance (mod 2^32) between the two timestamps needs to be
-            // less than half the timestamp space.
+            // newerFrame isn't newer than olderFrame, bail
             return;
         }
-        else if (tsDiff >= (-(1L << 30) & 0xFFFFFFFFL))
+        int lowestSeenSeqNumOfNewerFrame = newerFrame.getMinSeen();
+        int highestSeenSeqNumOfOlderFrame = olderFrame.getMaxSeen();
+        int seqNumDiff =
+                RTPUtils.sequenceNumberDiff(lowestSeenSeqNumOfNewerFrame, highestSeenSeqNumOfOlderFrame);
+
+        boolean guessed = false;
+
+        // We have some codecs (VPX) for which we can inspect the payload
+        // to deduce the start/end of olderFrame frame. This has an impact on the
+        // distances between the frames that allow us to guess frame
+        // boundaries.
+        //FIXME(brian): why does this allow us to infer anything extra from the sequence number
+        // differences?  we don't leverage frame boundary markings in any way when we deduce
+        // first/last sequence numbers.  does it just mean that these frames would've already
+        // (conclusively) had the start/end seq numbers set?  if so, we wouldn't need to
+        // call this method at all for those frames, right?
+        boolean framesSupportFrameBoundaries =
+            olderFrame.supportsFrameBoundaries() && newerFrame.supportsFrameBoundaries();
+
+        if (framesSupportFrameBoundaries)
         {
-            logger.warn("Frames that are out of order detected.");
-        }
-        else
-        {
-            int bMinSeqNum = b.getMinSeen(), aMaxSeqNum = a.getMaxSeen();
-            int snDiff = (bMinSeqNum - aMaxSeqNum) & 0xFFFF;
-
-            boolean guessed = false;
-
-            // We have some codecs (VPX) for which we can inspect the payload
-            // to deduce the start/end of a frame. This has an impact on the
-            // distances between the frames that allow us to guess frame
-            // boundaries.
-            boolean withFrameBoundaries =
-                a.supportsFrameBoundaries() && b.supportsFrameBoundaries();
-
-            if (withFrameBoundaries)
+            if (olderFrame.lastSequenceNumberKnown() || newerFrame.firstSequenceNumberKnown())
             {
-                if (bFirstSeqNum != -1 || aLastSeqNum != -1)
+                // XXX(bgrozev): for VPX codecs with PictureID we could find
+                // the start/end even with diff>2 (if PictureIDDiff == 1)
+
+                // XXX(gp): we don't have the picture ID in FrameDesc and I
+                // feel it doesn't belong there. We may need to subclass it
+                // into VPXFrameDesc and H264FrameDesc and move the
+                // heuristics logic in there.
+
+                if (seqNumDiff == 2)
                 {
-                    // XXX(bgrozev): for VPX codecs with PictureID we could find
-                    // the start/end even with diff>2 (if PictureIDDiff == 1)
-
-                    // XXX(gp): we don't have the picture ID in FrameDesc and I
-                    // feel it doesn't belong there. We may need to subclass it
-                    // into VPXFrameDesc and H264FrameDesc and move the
-                    // heuristics logic in there.
-
-                    if (snDiff == 2)
+                    if (!olderFrame.lastSequenceNumberKnown())
                     {
-                        if (aLastSeqNum == -1)
-                        {
-                            a.setEnd((aMaxSeqNum + 1) & 0xFFFF);
-                        }
-                        else
-                        {
-                            b.setStart((bMinSeqNum - 1) & 0xFFFF);
-                        }
-
-                        guessed = true;
+                        olderFrame.setEnd(RTPUtils.as16Bits(highestSeenSeqNumOfOlderFrame + 1));
                     }
-                }
-                else // (bFirstSeqNum == -1 && aLastSeqNum == -1)
-                {
-                    if (snDiff == 3)
+                    else
                     {
-                        a.setEnd((aMaxSeqNum + 1) & 0xFFFF);
-                        b.setStart((bMinSeqNum - 1) & 0xFFFF);
-                        guessed = true;
+                        newerFrame.setStart(RTPUtils.as16Bits(lowestSeenSeqNumOfNewerFrame - 1));
                     }
+                    guessed = true;
                 }
             }
             else
             {
-                if (bFirstSeqNum != -1 || aLastSeqNum != -1)
+                if (seqNumDiff == 3)
                 {
-                    if (snDiff == 1)
-                    {
-                        if (aLastSeqNum == -1)
-                        {
-                            a.setEnd(aMaxSeqNum & 0xFFFF);
-                        }
-                        else
-                        {
-                            b.setStart(bMinSeqNum & 0xFFFF);
-                        }
-
-                        guessed = true;
-                    }
-                }
-                else // (bFirstSeqNum == -1 && aLastSeqNum == -1)
-                {
-                    // XXX(bgrozev): Can't do much here. If diff==2 and
-                    // (bFirstSeqNum == -1 && aLastSeqNum == -1), then there is
-                    // 1 packet between aLastSeen and bFirstSeen. And we don't
-                    // know whether this packet belongs to a or to b, or is a
-                    // separate frame of it's own (since in this if branch there
-                    // is no support for frame boundaries, which means that e.g.
-                    // aLastSeen could be the end of a even if aLastSeqNum == -1).
+                    olderFrame.setEnd(RTPUtils.as16Bits(highestSeenSeqNumOfOlderFrame + 1));
+                    newerFrame.setStart(RTPUtils.as16Bits(lowestSeenSeqNumOfNewerFrame - 1));
+                    guessed = true;
                 }
             }
-
-            if (guessed)
+        }
+        else
+        {
+            if (olderFrame.lastSequenceNumberKnown() || newerFrame.firstSequenceNumberKnown())
             {
-                if (logger.isDebugEnabled())
+                if (seqNumDiff == 1)
                 {
-                    logger.debug(
-                        "Guessed frame boundaries ts=" + a.getTimestamp()
-                            + ",start=" + a.getStart()
-                            + ",end=" + a.getEnd()
-                            + ",ts=" + b.getTimestamp()
-                            + ",start=" + b.getStart()
-                            + ",end=" + b.getEnd());
+                    if (!olderFrame.lastSequenceNumberKnown())
+                    {
+                        olderFrame.setEnd(RTPUtils.as16Bits(highestSeenSeqNumOfOlderFrame));
+                    }
+                    else
+                    {
+                        newerFrame.setStart(RTPUtils.as16Bits(lowestSeenSeqNumOfNewerFrame));
+                    }
+
+                    guessed = true;
                 }
+            }
+            else
+            {
+                // XXX(bgrozev): Can't do much here. If diff==2 and
+                // we don't know either the first sequence number of the newer frame or
+                // the last sequence number of the older frame, then there is
+                // 1 packet between olderFrameLastSeen and newerFrameFirstSeen. And we don't
+                // know whether this packet belongs to olderFrame or to newerFrame, or is olderFrame
+                // separate frame of its own (since in this if branch there
+                // is no support for frame boundaries, which means that e.g.
+                // olderFrameLastSeen could be the end of olderFrame even if lastSeqNumOfOlderFrame == -1).
+            }
+        }
+
+        if (guessed)
+        {
+            if (logger.isDebugEnabled())
+            {
+                logger.debug(
+                    "Guessed frame boundaries ts=" + olderFrame.getTimestamp()
+                        + ",start=" + olderFrame.getStart()
+                        + ",end=" + olderFrame.getEnd()
+                        + ",ts=" + newerFrame.getTimestamp()
+                        + ",start=" + newerFrame.getStart()
+                        + ",end=" + newerFrame.getEnd());
             }
         }
     }

src/org/jitsi/impl/neomedia/rtp/TimestampUtils.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright @ 2017 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.impl.neomedia.rtp;
+
+import org.jitsi.util.RTPUtils;
+
+/**
+ * Helper class to perform various timestamp manipulations and comparisons
+ * @author Ethan Lin
+ * //FIXME(brian): perhaps this should be moved alongside RTPUtils (or maybe its
+ * contents combined with RTPUtils?  This class deals with RTP timestamps)
+ */
+public class TimestampUtils
+{
+    // RTP timestamp is 32 bits, this represents the maximum value of an RTP timestamp
+    public static final long MAX_TIMESTAMP_VALUE = Long.MAX_VALUE & 0xFFFFFFFFL;
+    // (Roughly) half of the MAX_TIMESTAMP_VALUE.  Used when we're trying to compare
+    // two timestamps to determine which came 'first'.
+    public static final long ROLLOVER_DELTA_VALUE = 0x80000000L;
+    /**
+     * Calculate the subtraction result of two long input as unsigned 32bit int.
+     *
+     * @param t1 the first timestamp
+     * @param t2 the second timestamp
+     * @return
+     */
+    public static long subtractAsUnsignedInt32(long t1, long t2)
+    {
+        return RTPUtils.as32Bits(t1 - t2);
+    }
+
+    /**
+     * Returns true if t1 is newer than t2,
+     * taking into account rollover.  This is done by effectively
+     * checking if the distance of going from 't2' to
+     * 't1' (strictly incrementing) is shorter or if going from
+     * 't1' to 't2' (i.e. rolling over) is shorter.
+     * webrtc/modules/include/module_common_types.h
+     *
+     * @param t1
+     * @param t2
+     * @return true if t1 is newer
+     */
+    public static boolean isNewerTimestamp(long t1, long t2)
+    {
+        if (t1 == t2)
+        {
+            return false;
+        }
+        // Distinguish between elements that are exactly ROLLOVER_DELTA_VALUE apart.
+        // If t1 > t2 and |t1-t2| == ROLLOVER_DELTA_VALUE:
+        // isNewerTimestamp(t1,t2) = true,
+        // isNewerTimestamp(t2,t1) = false
+        // rather than having:
+        // isNewerTimestamp(t1,t2) = isNewerTimestamp(t2,t1) = false.
+        if (subtractAsUnsignedInt32(t1, t2) == ROLLOVER_DELTA_VALUE)
+        {
+            // The two timestamps are exactly ROLLOVER_DELTA_VALUE apart, so
+            // we can't guess which is newer.  To break the tie, assume
+            // the larger timestamp is newer.
+            return t1 > t2;
+        }
+        return subtractAsUnsignedInt32(t1, t2) < ROLLOVER_DELTA_VALUE;
+    }
+
+    /**
+     * webrtc/modules/include/module_common_types.h
+     *
+     * @param timestamp1
+     * @param timestamp2
+     * @return
+     */
+    public static long latestTimestamp(long timestamp1, long timestamp2)
+    {
+        return
+            isNewerTimestamp(timestamp1, timestamp2) ? timestamp1 : timestamp2;
+    }
+}

src/org/jitsi/impl/neomedia/rtp/remotebitrateestimator/InterArrival.java
@@ -15,6 +15,7 @@
  */
 package org.jitsi.impl.neomedia.rtp.remotebitrateestimator;
 
+import org.jitsi.impl.neomedia.rtp.TimestampUtils;
 import org.jitsi.util.*;
 
 /**

src/org/jitsi/impl/neomedia/rtp/remotebitrateestimator/TimestampUtils.java
@@ -1,71 +1,0 @@
-/*
- * Copyright @ 2017 Atlassian Pty Ltd
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jitsi.impl.neomedia.rtp.remotebitrateestimator;
-
-/**
- * Helper class to determine timestamp order
- * @author Ethan Lin
- */
-class TimestampUtils
-{
-    /**
-     * Calculate the subtraction result of two long input as unsigned 32bit int.
-     *
-     * @param t1
-     * @param t2
-     * @return
-     */
-    static long subtractAsUnsignedInt32(long t1, long t2)
-    {
-        return (t1 - t2) & 0xFFFFFFFFL;
-    }
-
-    /**
-     * webrtc/modules/include/module_common_types.h
-     *
-     * @param timestamp
-     * @param prevTimestamp
-     * @return true if timestamp is newer
-     */
-    static boolean isNewerTimestamp(long timestamp, long prevTimestamp)
-    {
-        // Distinguish between elements that are exactly 0x80000000 apart.
-        // If t1>t2 and |t1-t2| = 0x80000000: IsNewer(t1,t2)=true,
-        // IsNewer(t2,t1)=false
-        // rather than having IsNewer(t1,t2) = IsNewer(t2,t1) = false.
-        if (subtractAsUnsignedInt32(timestamp, prevTimestamp) == 0x80000000L)
-        {
-            return timestamp > prevTimestamp;
-        }
-        return
-            timestamp != prevTimestamp
-                && subtractAsUnsignedInt32(timestamp, prevTimestamp) < 0x80000000L;
-    }
-
-    /**
-     * webrtc/modules/include/module_common_types.h
-     *
-     * @param timestamp1
-     * @param timestamp2
-     * @return
-     */
-    static long latestTimestamp(long timestamp1, long timestamp2)
-    {
-        return
-            isNewerTimestamp(timestamp1, timestamp2) ? timestamp1 : timestamp2;
-    }
-
-}

test/org/jitsi/impl/neomedia/rtp/remotebitrateestimator/TimestampUtilsTest.java
@@ -15,6 +15,7 @@
  */
 package org.jitsi.impl.neomedia.rtp.remotebitrateestimator;
 
+import org.jitsi.impl.neomedia.rtp.TimestampUtils;
 import org.junit.*;
 
 import static org.junit.Assert.*;

commit 62ba93f3842983519c0454df4dc9cdf4c2f7ecd2

fix: Fixes mod substraction regression introduced with fc767ef.

src/org/jitsi/util/RTPUtils.java
@@ -228,7 +228,7 @@ public class RTPUtils
      */
     public static long as32Bits(long value)
     {
-        return value & 0xFFFFFFFF;
+        return value & 0xFFFFFFFFL;
     }
 
     /**