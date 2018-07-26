252c78e3948c51e1236fb8fc004e3f03404ad11d
George Politis <gp@jitsi.org>
2018-07-26 15:36:38 -0500

    ref: Removes defunct probing detection code.
    We never had actual probing, so this detection code
    was either not working at all or not working as
    expected.

--- src/org/jitsi/impl/neomedia/rtp/remotebitrateestimator/RemoteBitrateEstimatorAbsSendTime.java
+++ src/org/jitsi/impl/neomedia/rtp/remotebitrateestimator/RemoteBitrateEstimatorAbsSendTime.java
@@ -18,7 +18,6 @@ package org.jitsi.impl.neomedia.rtp.remotebitrateestimator;
 
 import org.ice4j.util.*;
 import org.jitsi.service.neomedia.rtp.*;
-import org.jitsi.util.Logger;
 import org.jitsi.util.*;
 import org.jetbrains.annotations.*;
 
@@ -34,14 +33,6 @@ import java.util.*;
 public class RemoteBitrateEstimatorAbsSendTime
     implements RemoteBitrateEstimator
 {
-    /**
-     * The <tt>Logger</tt> used by the
-     * <tt>RemoteBitrateEstimatorAbsSendTime</tt> class and its instances for
-     * logging output.
-     */
-    private static final Logger logger
-        = Logger.getLogger(RemoteBitrateEstimatorAbsSendTime.class);
-
     /**
      * The {@link TimeSeriesLogger} to be used by this instance to print time
      * series.
@@ -92,41 +83,6 @@ public class RemoteBitrateEstimatorAbsSendTime
         kTimestampToMs = (double) 1000 / (1 << kInterArrivalShift);
 
     /**
-     *  Defines the maximum distance between the send time delta (of a probe
-     *  packet from the previous probe packet) and the cluster mean send time
-     *  delta above which the probe is no longer considered part of the cluster.
-     */
-    private final static float kMaxSendTimeDeltaMsDistance = 2.5f;
-
-    /**
-     * Defines the minimum size of a packet to be considered a probe. We
-     * currently assume that only packets larger than 200 bytes are paced by the
-     * sender.
-     */
-    private final static long kMinProbePacketSize = 200;
-
-    /**
-     * Defines the initial probing period (in millis) after the first packet is
-     * received.
-     */
-    private final static int kInitialProbingIntervalMs = 2000;
-
-    /**
-     * Defines the minimum cluster size for the cluster to be considered valid.
-     */
-    private final static int kMinClusterSize = 4;
-
-    /**
-     * Defines the maximum number of probe packets.
-     */
-    private final static int kMaxProbePackets = 15;
-
-    /**
-     * Defines the expected number of probe packets.
-     */
-    private final static int kExpectedNumberOfProbes = 3;
-
-    /**
      * Reduces the effects of allocations and garbage collection of the method
      * {@code incomingPacket}.
      */
@@ -157,16 +113,6 @@ public class RemoteBitrateEstimatorAbsSendTime
      * A map of SSRCs -> time first seen (in millis).
      */
     private final Map<Long, Long> ssrcsMap = new TreeMap<>();
-
-    /**
-     * The list of probes that this instance has received.
-     */
-    private final List<Probe> probes = new ArrayList<>();
-
-    /**
-     * The total number of probing packets we've seen so far.
-     */
-    private long totalProbesReceived;
 
     /**
      * The time (in millis) when we saw the first packet. Useful to determine
@@ -228,222 +174,8 @@ public class RemoteBitrateEstimatorAbsSendTime
         this.remoteRate = new AimdRateControl(diagnosticContext);
         this.incomingBitrate = new RateStatistics(kBitrateWindowMs, kBitrateScale);
         this.incomingBitrateInitialized = false;
-        this.totalProbesReceived = 0;
         this.firstPacketTimeMs = -1;
         this.lastUpdateMs = -1;
-    }
-
-    /**
-     * Determines whether a {@link Probe} belongs to a cluster or not, by
-     * examining its send time delta distance from the cluster mean.
-     *
-     * @param sendDeltaMs the send time delta of the probe we examine (probe
-     * send time - previous probe send time).
-     * @param clusterAggregate the {@link Cluster} that aggregates the probes.
-     *
-     * @return true if the probe is within the cluster bounds, false otherwise.
-     */
-    private static boolean isWithinClusterBounds(
-        long sendDeltaMs, Cluster clusterAggregate)
-    {
-        if (clusterAggregate.count == 0)
-        {
-            return true;
-        }
-        double clusterMean = clusterAggregate.meanSendDeltaMs /
-            (double) clusterAggregate.count;
-        
-        return Math.abs(
-            (double) sendDeltaMs - clusterMean) < kMaxSendTimeDeltaMsDistance;
-    }
-
-    /**
-     * Finalizes the cluster (computes the means from the sums) and adds it to
-     * the clusters list.
-     *
-     * @param clusters the clusters list to add the cluster.
-     * @param cluster the cluster to finalize and add to the clusters list.
-     */
-    private static void addCluster(List<Cluster> clusters, Cluster cluster)
-    {
-        cluster.meanSendDeltaMs /= (double) cluster.count;
-        cluster.meanRecvDeltaMs /= (double) cluster.count;
-        cluster.meanSize /= cluster.count;
-        clusters.add(cluster);
-    }
-
-    /**
-     * Computes the list of clusters from the list of probes.
-     *
-     * @param probes the list of probes
-     * @return the computed list of clusters.
-     */
-    private static List<Cluster> computeClusters(List<Probe> probes)
-    {
-        List<Cluster> clusters = new ArrayList<>();
-
-        Cluster current = new Cluster();
-        long prevSendTime = -1;
-        long prevRecvTime = -1;
-        for (Probe probe : probes)
-        {
-            if (prevSendTime >= 0)
-            {
-                long sendDeltaMs = probe.sendTimeMs - prevSendTime;
-                long recvDeltaMs = probe.recvTimeMs - prevRecvTime;
-
-                if (sendDeltaMs >= 1 && recvDeltaMs >= 1)
-                {
-                    ++current.numAboveMinDelta;
-                }
-
-                if (!isWithinClusterBounds(sendDeltaMs, current))
-                {
-                    if (current.count >= kMinClusterSize)
-                    {
-                        addCluster(clusters, current);
-                    }
-                    current = new Cluster();
-                }
-                current.meanSendDeltaMs += sendDeltaMs;
-                current.meanRecvDeltaMs += recvDeltaMs;
-                current.meanSize += probe.payloadSize;
-                ++current.count;
-            }
-
-            prevSendTime = probe.sendTimeMs;
-            prevRecvTime = probe.recvTimeMs;
-        }
-
-        if (current.count >= kMinClusterSize)
-        {
-            addCluster(clusters, current);
-        }
-
-        return clusters;
-    }
-
-    /**
-     * Finds the probe cluster with the highest bitrate.
-     *
-     * @param clusters the list of clusters
-     * @return the cluster with the highest bitrate
-     */
-    private static Cluster findBestProbe(List<Cluster> clusters)
-    {
-        long highestProbeBitrateBps = 0;
-        Cluster bestIt = new Cluster();
-        for (Cluster cluster : clusters)
-        {
-            if (cluster.meanSendDeltaMs == 0 || cluster.meanRecvDeltaMs == 0)
-                continue;
-            if (cluster.numAboveMinDelta > cluster.count / 2 &&
-                (cluster.meanRecvDeltaMs - cluster.meanSendDeltaMs <= 2.0f &&
-                    cluster.meanSendDeltaMs - cluster.meanRecvDeltaMs <= 5.0f))
-            {
-                long probeBitrateBps = Math.min(
-                    cluster.getSendBitrateBps(), cluster.getRecvBitrateBps());
-
-                if (probeBitrateBps > highestProbeBitrateBps)
-                {
-                    highestProbeBitrateBps = probeBitrateBps;
-                    bestIt = cluster;
-                }
-            }
-            else
-            {
-                double sendBitrateBps = cluster.meanSize * 8 * 1000
-                    / cluster.meanSendDeltaMs;
-                double recvBitrateBps = cluster.meanSize * 8 * 1000
-                    / cluster.meanRecvDeltaMs;
-                if (logger.isDebugEnabled())
-                {
-                    logger.debug(
-                        "Probe failed, sent at " + sendBitrateBps
-                            + " bps, received at " + recvBitrateBps
-                            + " bps. Mean send delta: " + cluster.meanSendDeltaMs
-                            + " ms, mean recv delta: " + cluster.meanRecvDeltaMs
-                            + " ms, num probes: " + cluster.count);
-                }
-                break;
-            }
-        }
-        return bestIt;
-    }
-
-    /**
-     * Processes the received clusters and maybe updates the remote bitrate.
-     * It returns the processing result.
-     *
-     * @param nowMs the current time in millis.
-     *
-     * @return true if the remote bitrate was updated, false otherwise.
-     */
-    private synchronized boolean processClusters(long nowMs)
-    {
-        List<Cluster> clusters = computeClusters(probes);
-        if (clusters.isEmpty())
-        {
-            // If we reach the max number of probe packets and still
-            // have no clusters, we will remove the oldest one.
-            if (probes.size() >= kMaxProbePackets)
-            {
-                probes.remove(0);
-            }
-            return false;
-        }
-
-        Cluster bestProbe = findBestProbe(clusters);
-        long probeBitrateBps = Math.min(
-            bestProbe.getSendBitrateBps(), bestProbe.getRecvBitrateBps());
-
-        // Make sure that a probe sent on a lower bitrate than our estimate
-        // can't reduce the estimate.
-
-        if (isBitrateImproving(probeBitrateBps))
-        {
-            if (logger.isDebugEnabled())
-            {
-                logger.debug(
-                    "Probe successful, sent at "
-                        + bestProbe.getSendBitrateBps()
-                        + " bps, received at " + bestProbe.getRecvBitrateBps()
-                        + " bps. Mean send delta: " + bestProbe.meanSendDeltaMs
-                        + " ms, mean recv delta: " + bestProbe.meanRecvDeltaMs
-                        + " ms, num probes: " + bestProbe.count);
-            }
-            remoteRate.setEstimate(probeBitrateBps, nowMs);
-            return true;
-        }
-
-        // Not probing and received non-probe packet, or finished with current
-        // set of probes.
-
-        if (clusters.size() >= kExpectedNumberOfProbes)
-        {
-            probes.clear();
-        }
-
-        return false;
-    }
-
-    /**
-     * Determines whether or not the specified bitrate is an improvement over
-     * our current estimate.
-     *
-     * @param newBitrateBps the new bitrate to compare with our estimate.
-     *
-     * @return true if the bitrate is improving, false otherwise.
-     */
-    private synchronized boolean isBitrateImproving(long newBitrateBps)
-    {
-        boolean initialProbe
-            = !remoteRate.isValidEstimate() && newBitrateBps > 0;
-
-        boolean bitrateAboveEstimate = remoteRate.isValidEstimate()
-            && newBitrateBps > remoteRate.getLatestEstimate();
-
-        return initialProbe || bitrateAboveEstimate;
     }
 
     /**
@@ -522,45 +254,6 @@ public class RemoteBitrateEstimatorAbsSendTime
                 ssrcs = Collections.unmodifiableCollection(ssrcsMap.keySet());
             }
 
-            // For now only try to detect probes while we don't have a valid
-            // estimate.
-            if (payloadSize > kMinProbePacketSize &&
-                (!remoteRate.isValidEstimate() ||
-                    nowMs - firstPacketTimeMs < kInitialProbingIntervalMs))
-            {
-                if (totalProbesReceived < kMaxProbePackets)
-                {
-                    long sendDeltaMs = -1;
-                    long recvDeltaMs = -1;
-                    if (!probes.isEmpty())
-                    {
-                        sendDeltaMs = sendTimeMs - probes
-                            .get(probes.size() - 1).sendTimeMs;
-                        recvDeltaMs = arrivalTimeMs - probes
-                            .get(probes.size() - 1).recvTimeMs;
-                    }
-                    if (logger.isDebugEnabled())
-                    {
-                        logger.debug(
-                            "Probe packet received: send time=" + sendTimeMs
-                                    + " ms, recv time=" + arrivalTimeMs
-                                    + " ms, send delta=" + sendDeltaMs
-                                    + " ms, recv delta=" + recvDeltaMs + " ms.");
-                    }
-                }
-                probes.add(
-                    new Probe(sendTimeMs, arrivalTimeMs, payloadSize));
-
-                ++totalProbesReceived;
-                // Make sure that a probe which updated the bitrate immediately
-                // has an effect by calling the
-                // OnReceiveBitrateChanged callback.
-                if (processClusters(nowMs))
-                {
-                    updateEstimate = true;
-                }
-            }
-
             long[] deltas = this.deltas;
 
             /* long timestampDelta */ deltas[0] = 0;
@@ -588,25 +281,22 @@ public class RemoteBitrateEstimatorAbsSendTime
                     detector.estimator.getNumOfDeltas(), arrivalTimeMs);
             }
 
-            if (!updateEstimate)
+            // Check if it's time for a periodic update or if we
+            // should update because of an over-use.
+            if (lastUpdateMs == -1
+                || nowMs - lastUpdateMs > remoteRate.getFeedBackInterval())
             {
-                // Check if it's time for a periodic update or if we
-                // should update because of an over-use.
-                if (lastUpdateMs == -1
-                    || nowMs - lastUpdateMs > remoteRate.getFeedBackInterval())
+                updateEstimate = true;
+            }
+            else if (
+                detector.detector.getState() == BandwidthUsage.kBwOverusing)
+            {
+                long incomingRate_ = incomingBitrate.getRate(arrivalTimeMs);
+
+                if (incomingRate_ > 0 && remoteRate
+                    .isTimeToReduceFurther(nowMs, incomingBitrate_))
                 {
                     updateEstimate = true;
-                }
-                else if (
-                    detector.detector.getState() == BandwidthUsage.kBwOverusing)
-                {
-                    long incomingRate_ = incomingBitrate.getRate(arrivalTimeMs);
-
-                    if (incomingRate_ > 0 && remoteRate
-                        .isTimeToReduceFurther(nowMs, incomingBitrate_))
-                    {
-                        updateEstimate = true;
-                    }
                 }
             }
 
@@ -735,50 +425,6 @@ public class RemoteBitrateEstimatorAbsSendTime
     }
 
     /**
-     * Keeps meta information about a cluster of probes.
-     */
-    private static class Cluster
-    {
-        double meanSendDeltaMs = 0L;
-        double meanRecvDeltaMs = 0L;
-        int meanSize = 0;
-        int count = 0;
-        int numAboveMinDelta = 0;
-
-        Cluster() {
-        }
-
-        long getSendBitrateBps()
-        {
-            //RTC_CHECK_GT(this.meanSendDeltaMs, 0.0f);
-            return (long) (this.meanSize * 8 * 1000 / meanSendDeltaMs);
-        }
-
-        long getRecvBitrateBps()
-        {
-            // RTC_CHECK_GT(this.meanRecvDeltaMs, 0.0f);
-            return (long) (this.meanSize * 8 * 1000 / this.meanRecvDeltaMs);
-        }
-    }
-
-    /**
-     * Keeps meta information about a probe packet.
-     */
-    private class Probe
-    {
-        long sendTimeMs = -1L;
-        long recvTimeMs = -1L;
-        long payloadSize = 0;
-
-        Probe(long sendTimeMs, long recvTimeMs, long payloadSize)
-        {
-            this.sendTimeMs = sendTimeMs;
-            this.recvTimeMs = recvTimeMs;
-            this.payloadSize = payloadSize;
-        }
-    }
-
-    /**
      * Holds the {@link InterArrival}, {@link OveruseEstimator} and
      * {@link OveruseDetector} instances that estimate the remote bitrate of a
      * stream.
@@ -803,11 +449,12 @@ public class RemoteBitrateEstimatorAbsSendTime
         /**
          * Ctor.
          *
-         * @param options
-         * @param enableBurstGrouping
+         * @param options the over-use detector options.
+         * @param enableBurstGrouping true to activate burst detection, false
+         * otherwise
          */
-        public Detector(OverUseDetectorOptions options,
-            boolean enableBurstGrouping)
+        Detector(OverUseDetectorOptions options,
+                 boolean enableBurstGrouping)
         {
             this.interArrival = new InterArrival(
                 kTimestampGroupLengthTicks, kTimestampToMs,
