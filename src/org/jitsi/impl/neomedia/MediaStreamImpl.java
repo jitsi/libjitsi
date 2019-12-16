jitsi/libjitsi

commit f60f7819c5750643ee6a2732cc7316fcb0178fd6

Update translator mode (#465)
* Updates connector to throw IllegalStateException when closed.

The change is as discussed in https://github.com/jitsi/libjitsi/pull/464

* Skips trying to playback streams when using translator.

In case of jvb the device session is null. In case of jigasi we have a conference call with a device which is ticking to be able transfer data (AudioSilenceCaptureDevice) but we are using a translator to send the streams without decoding.

* Audio device session to ignores audio effects when translator is in use.

src/org/jitsi/impl/neomedia/MediaStreamImpl.java
@@ -608,7 +608,7 @@ public class MediaStreamImpl
             {
                 MediaDeviceSession deviceSession = getDeviceSession();
 
-                if (deviceSession == null)
+                if (deviceSession == null || rtpTranslator != null)
                 {
                     // Since there is no output MediaDevice to render the
                     // receiveStream on, the JitterBuffer of the receiveStream
@@ -4249,6 +4249,20 @@ public class MediaStreamImpl
         if (transportCCEngine != null)
         {
             transportCCEngine.addMediaStream(this);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setRTPTranslator(RTPTranslator rtpTranslator)
+    {
+        super.setRTPTranslator(rtpTranslator);
+
+        if (this.deviceSession != null)
+        {
+            this.deviceSession.setUseTranslator(rtpTranslator != null);
         }
     }
 }

src/org/jitsi/impl/neomedia/device/AudioMediaDeviceSession.java
@@ -199,6 +199,12 @@ public class AudioMediaDeviceSession
     {
         super.processorControllerUpdate(event);
 
+        // when using translator we do not want any audio level effect
+        if (useTranslator)
+        {
+            return;
+        }
+
         if (event instanceof ConfigureCompleteEvent)
         {
             Processor processor = (Processor) event.getSourceController();
@@ -289,6 +295,11 @@ public class AudioMediaDeviceSession
     public void setLocalUserAudioLevelListener(
             SimpleAudioLevelListener listener)
     {
+        if (useTranslator)
+        {
+            return;
+        }
+
         localUserAudioLevelEffect.setAudioLevelListener(listener);
     }
 
@@ -320,6 +331,10 @@ public class AudioMediaDeviceSession
      */
     public void setStreamAudioLevelListener(SimpleAudioLevelListener listener)
     {
+        if (useTranslator)
+        {
+            return;
+        }
         streamAudioLevelEffect.setAudioLevelListener(listener);
     }
 
@@ -359,6 +374,13 @@ public class AudioMediaDeviceSession
     protected Processor createProcessor()
     {
         Processor processor = super.createProcessor();
+
+        // when using translator we do not want any audio level effect
+        if(useTranslator)
+        {
+            return processor;
+        }
+
         if (processor != null)
         {
             if (outputAudioLevelEffect != null)

src/org/jitsi/impl/neomedia/device/MediaDeviceSession.java
@@ -206,6 +206,11 @@ public class MediaDeviceSession
     protected boolean outputSizeChanged = false;
 
     /**
+     * Whether this device session is used by a stream which uses a translator.
+     */
+    public boolean useTranslator = false;
+
+    /**
      * Initializes a new <tt>MediaDeviceSession</tt> instance which is to
      * represent the use of a specific <tt>MediaDevice</tt> by a
      * <tt>MediaStream</tt>.
@@ -2556,5 +2561,15 @@ public class MediaDeviceSession
         if (controls == null)
             controls = Collections.emptySet();
         return controls;
+    }
+
+    /**
+     * Updates the value of <tt>useTranslator</tt>.
+     * @param useTranslator whether this device session is used by
+     * a <tt>MediaStream</tt> that is having a translator.
+     */
+    public void setUseTranslator(boolean useTranslator)
+    {
+        this.useTranslator = useTranslator;
     }
 }

src/org/jitsi/impl/neomedia/rtp/translator/RTPConnectorImpl.java
@@ -211,7 +211,7 @@ class RTPConnectorImpl
     {
         if (this.closed)
         {
-            return null;
+            throw new IllegalStateException("Connector closed.");
         }
 
         if (this.controlOutputStream == null)
@@ -262,7 +262,7 @@ class RTPConnectorImpl
     {
         if (this.closed)
         {
-            return null;
+            throw new IllegalStateException("Connector closed.");
         }
 
         if (this.dataOutputStream == null)

commit f4145abb785ea1958314d3ef8fea054f4b92dd02

MediaStream: Re-enable incoming video.
caused by commit f60f781, undoing just the culprit

fixes jitsi/jitsi#627

src/org/jitsi/impl/neomedia/MediaStreamImpl.java
@@ -611,7 +611,7 @@ public class MediaStreamImpl
             {
                 MediaDeviceSession deviceSession = getDeviceSession();
 
-                if (deviceSession == null || rtpTranslator != null)
+                if (deviceSession == null)
                 {
                     // Since there is no output MediaDevice to render the
                     // receiveStream on, the JitterBuffer of the receiveStream