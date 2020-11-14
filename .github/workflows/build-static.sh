abc372c05515562fdbfa88c68f002facbaee1364
Ingo Bauersachs <ingo@jitsi.org>
2020-11-14 18:35:05 +0100

    Fix vcpkg build

--- .github/workflows/build-static.sh
+++ .github/workflows/build-static.sh
@@ -2,7 +2,7 @@ git config --global user.email "jitsi-jenkins@jitsi.org"
 git config --global user.name "Jitsi GitHub Action"
 
 ARCH=$1
-DIR=$2
+LIBROOT=$2
 VCPKG_ARCH="x64"
 JAVA_ARCH="amd64"
 if [ "$ARCH" == "m32" ]; then
@@ -11,13 +11,15 @@ if [ "$ARCH" == "m32" ]; then
 fi
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-$JAVA_ARCH
-export CFLAGS=-$ARCH
-export CXXFLAGS=-$ARCH
+export CFLAGS="-$ARCH -fPIC"
+export CXXFLAGS="-$ARCH -fPIC"
 
-cd $DIR/src/native || exit 1
+cd "$LIBROOT/src/native" || exit 1
+cp x86-linux.cmake vcpkg/triplets
 cmake -B cmake-build-$ARCH \
-  -DCMAKE_C_FLAGS=-$ARCH \
-  -DCMAKE_CXX_FLAGS=-$ARCH \
+  -DVCPKG_VERBOSE=ON \
+  -DCMAKE_C_FLAGS="$CFLAGS" \
+  -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
   -DVCPKG_TARGET_TRIPLET=$VCPKG_ARCH-linux \
   -DUSE_SYSTEM_OPUS=OFF \
   -DUSE_SYSTEM_SPEEX=OFF \
