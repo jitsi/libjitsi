2d509020ddcd4cee8b0a8bb9df311e36ebde0962
Ingo Bauersachs <ingo@jitsi.org>
2023-02-26 21:42:56 +0100

    Remove unsupported filter for speex and speexdsp

--- resources/ubuntu-build-image/build-static.sh
+++ resources/ubuntu-build-image/build-static.sh
@@ -36,7 +36,6 @@ esac
 export JAVA_HOME=/usr/lib/jvm/java-$JAVA_VERSION-openjdk-$JAVA_ARCH
 
 cd "$LIBROOT/src/native" || exit 1
-cp cmake/vcpkg-triplets/x86-linux.cmake vcpkg/triplets/community
 cmake -B cmake-build-$ARCH \
   -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$TOOLCHAIN \
   -DVCPKG_VERBOSE=ON \
