e9510509a34b02e5ffd469c862916e650438f29e
Ingo Bauersachs <ingo@jitsi.org>
2025-04-27 18:01:12 +0200

    Add Java 21, unlock sbuild group

--- resources/deb-build.sh
+++ resources/deb-build.sh
@@ -10,23 +10,19 @@ cd "${PROJECT_DIR}" || exit
 export BUILD_DIR=${PROJECT_DIR}/target/debian/${DIST}
 mkdir -p "${BUILD_DIR}"
 
-# https://bugs.launchpad.net/ubuntu/+source/ubuntu-dev-tools/+bug/1964670
-sudo sed -i s/pkg-config-\$target_tuple//g /usr/bin/mk-sbuild
-
 # use tmpfs for sbuild
 sudo tee -a /etc/fstab < "${PROJECT_DIR}/resources/sbuild-tmpfs"
 
 # --skip-security because: https://bugs.launchpad.net/ubuntu/+source/ubuntu-dev-tools/+bug/1955116
-# -debootstrap-include=default-jdk because: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=994152
 if [[ "${ARCH}" != "amd64" ]]; then
-  mk-sbuild "${DIST}" --target "${ARCH}" --skip-security --type=file --debootstrap-include=default-jdk || sbuild-update -udc "${DIST}"-amd64-"${ARCH}"
+  mk-sbuild "${DIST}" --target "${ARCH}" --skip-security --type=file --debootstrap-include=ca-certificates || sbuild-update -udc "${DIST}"-amd64-"${ARCH}"
 else
   if debian-distro-info --all | grep -Fqxi "${DIST}"; then
     export DEBOOTSTRAP_MIRROR=${DEBOOTSTRAP_MIRROR:-$UBUNTUTOOLS_DEBIAN_MIRROR}
   elif ubuntu-distro-info --all | grep -Fqxi "${DIST}"; then
     export DEBOOTSTRAP_MIRROR=${DEBOOTSTRAP_MIRROR:-$UBUNTUTOOLS_UBUNTU_MIRROR}
   fi
-  mk-sbuild "${DIST}" --skip-security --type=file --debootstrap-include=default-jdk || sbuild-update -udc "${DIST}"-amd64
+  mk-sbuild "${DIST}" --skip-security --type=file --debootstrap-include=ca-certificates || sbuild-update -udc "${DIST}"-amd64
 fi
 
 mvn -B versions:set -DnewVersion="${VERSION}" -DgenerateBackupPoms=false
