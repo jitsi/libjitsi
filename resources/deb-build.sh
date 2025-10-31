580212eb383936ac7589dd39063bfbbd5c9c4381
damencho <damencho@jitsi.org>
2025-10-31 16:01:41 -0500

    Fixed. Changed from a string variable to a bash array (EXTRA_REPO_ARGS=()) and used "${EXTRA_REPO_ARGS[@]}" expansion so each --extra-repository argument is properly quoted and passed as a single argument to   sbuild.

--- resources/deb-build.sh
+++ resources/deb-build.sh
@@ -76,15 +76,17 @@ mvn -B versions:set -DnewVersion="${VERSION}" -DgenerateBackupPoms=false
 export SBUILD_CONFIG="${PROJECT_DIR}/resources/sbuildrc"
 if [[ "${ARCH}" != "amd64" ]]; then
   # Add ports repository for cross-compilation
-  EXTRA_REPOS=""
+  EXTRA_REPO_ARGS=()
   if [[ "${ARCH}" == "arm64" || "${ARCH}" == "ppc64el" ]]; then
     if ubuntu-distro-info --all | grep -Fqxi "${DIST}"; then
-      EXTRA_REPOS="--extra-repository=deb [arch=arm64,ppc64el] http://ports.ubuntu.com/ubuntu-ports ${DIST} main universe --extra-repository=deb [arch=arm64,ppc64el] http://ports.ubuntu.com/ubuntu-ports ${DIST}-updates main universe"
+      EXTRA_REPO_ARGS+=("--extra-repository=deb [arch=arm64,ppc64el] http://ports.ubuntu.com/ubuntu-ports ${DIST} main universe")
+      EXTRA_REPO_ARGS+=("--extra-repository=deb [arch=arm64,ppc64el] http://ports.ubuntu.com/ubuntu-ports ${DIST}-updates main universe")
     elif debian-distro-info --all | grep -Fqxi "${DIST}"; then
-      EXTRA_REPOS="--extra-repository=deb [arch=arm64,ppc64el] http://deb.debian.org/debian ${DIST} main --extra-repository=deb [arch=arm64,ppc64el] http://deb.debian.org/debian ${DIST}-updates main"
+      EXTRA_REPO_ARGS+=("--extra-repository=deb [arch=arm64,ppc64el] http://deb.debian.org/debian ${DIST} main")
+      EXTRA_REPO_ARGS+=("--extra-repository=deb [arch=arm64,ppc64el] http://deb.debian.org/debian ${DIST}-updates main")
     fi
   fi
-  sbuild --dist "${DIST}" --no-arch-all --host "${ARCH}" --build=amd64 --no-apt-distupgrade --bd-uninstallable-explainer=none ${EXTRA_REPOS} "${PROJECT_DIR}"/../libjitsi_*.dsc
+  sbuild --dist "${DIST}" --no-arch-all --host "${ARCH}" --build=amd64 --no-apt-distupgrade --bd-uninstallable-explainer=none "${EXTRA_REPO_ARGS[@]}" "${PROJECT_DIR}"/../libjitsi_*.dsc
 else
   sbuild --dist "${DIST}" --arch-all "${PROJECT_DIR}"/../libjitsi_*.dsc
   cp "${PROJECT_DIR}"/../libjitsi_* "$BUILD_DIR"
